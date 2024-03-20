4a6db6eea54c3ef38de3d1df2b0a952c08dae761
Mikaele Mendonça <mikaelemendonca@gmail.com>
2024-03-19 23:09:12 -0300

    Liskov Subistitution Principle

--- src/Model/AluraMais.php
+++ src/Model/AluraMais.php
@@ -16,7 +16,7 @@ class AluraMais extends Video implements Pontuavel
 
     public function recuperarUrl(): string
     {
-        return str_replace(' ', '-', strtolower($this->categoria));
+        return 'http://videos.alura.com.br/' . str_replace(' ', '-', strtolower($this->categoria));
     }
 
     public function pontuacao(): int
